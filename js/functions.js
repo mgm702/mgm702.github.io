3900f67828c2b9484c0bb1bcf8103d679e2a69d1
Matt Michnal <mattm3646@gmail.com>
2015-03-07 12:25:49 -0800

    slick carousel plugin function added to js file.

--- js/functions.js
+++ js/functions.js
@@ -26,11 +26,10 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 var cbpAnimatedHeader = (function() {
-
   var docElem = document.documentElement,
   header = document.querySelector( '.navbar-default' ),
   didScroll = false,
-  changeHeaderOn = 300;
+  changeHeaderOn = 100;
 
   function init() {
     window.addEventListener( 'scroll', function( event ) {
@@ -46,10 +45,12 @@ var cbpAnimatedHeader = (function() {
     if ( sy >= changeHeaderOn ) {
       classie.add( header, 'navbar-shrink' );
       $("nav.navbar.navbar-default.navbar-fixed-top").css("background", "black");
+      $("a.navbar-brand.page-scroll").css("color","#E53E00");
     }
     else {
       classie.remove( header, 'navbar-shrink' );
       $("nav.navbar.navbar-default.navbar-fixed-top").css("background", "none");
+      $("a.navbar-brand.page-scroll").css("color", "#fff");
     }
     didScroll = false;
   }
@@ -60,3 +61,49 @@ var cbpAnimatedHeader = (function() {
 
   init();
 })();
+
+$(document).ready(function(){
+  $('.col-md-10.col-md-offset-1.software').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    fade: false,
+    easing: 'easeInOutCubic',
+    responsive:
+    [
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    }
+    ]
+  });
+});
+
+function sendMail() {
+  var link = "mailto:mattm3646@gmail.com";
+  link += "?subject=" + escape("Project Idea");
+  link += "&body=" + escape(document.getElementById('bodyText').value);
+
+  window.location.href = link;
+}
+/*
+$(".col-lg-3.text-center.repo").hover(function(){
+  $(this).stop(true, true).css("background-color","#E53E00");
+  $(this).css("color","black");
+  $("#gh_repo a").css("color","black");
+}, function(){
+  $(this).stop(true, true).css("background-color","#2f2f2f");
+  $(this).css("color","white");
+  $("#gh_repo a").css("color","black");
+});
+*/
