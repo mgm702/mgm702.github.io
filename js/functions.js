8154747d1cafcb47e5b06d4ddce1929f7263f4b5
Matt Michnal <mattm3646@gmail.com>
2015-03-19 14:38:01 -0700

    trying touchstart instead of touchend for triggered event.

--- js/functions.js
+++ js/functions.js
@@ -103,7 +103,7 @@ $('#responsive-menu-button').sidr({
 
 });
 
-$(document).on('click touchend', 'a.btn.btn-default.mobile-btn', function(event) {
+$(document).on('click touchstart', 'a.btn.btn-default.mobile-btn', function(event) {
   event.preventDefault();
   $("#responsive-menu-button").click();
 });
